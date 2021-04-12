2aa805c3a0a929ad873d15ff81b6c3633417d117
dgartin6374 <54429706+dgartin6374@users.noreply.github.com>
2021-04-12 19:10:49 -0400

    Basic Walling And Enemy AI

---
+++ Assets/Scripts/Enemy Scripts/RandomMove.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMove : MonoBehaviour
+{
+    private float latestDirectionChangeTime;
+    private float directionChangeTime = 3f;
+    private float characterVelocity = 0.5f;
+    private Vector2 movementDirection;
+    private Vector2 movementPerSecond;
+
+
+    void Start()
+    {
+        latestDirectionChangeTime = 0f;
+        calcuateNewMovementVector();
+    }
+
+    void calcuateNewMovementVector()
+    {
+        //create a random direction vector with the magnitude of 1, later multiply it with the velocity of the enemy
+        movementDirection = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)).normalized;
+        movementPerSecond = movementDirection * characterVelocity;
+    }
+
+    void Update()
+    {
+        //if the changeTime was reached, calculate a new movement vector
+        if (Time.time - latestDirectionChangeTime > directionChangeTime)
+        {
+            latestDirectionChangeTime = Time.time;
+            directionChangeTime = Random.Range(0f, 5.0f); // THIS IS USED TO MAKE THE RANDOM CODE HAPPEN ON A RANDOM INTERVAL
+            calcuateNewMovementVector();
+        }
+
+        //move enemy: 
+        transform.position = new Vector2(transform.position.x + (movementPerSecond.x * Time.deltaTime),
+        transform.position.y + (movementPerSecond.y * Time.deltaTime));
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) // This Is used to Check If hitting a wall Area and then To Turn Back 
+    {
+        if (other.gameObject.tag == "Basic Walling" )
+        {
+            Debug.Log("COLLISION  WALL"); // Used Currently In Testing
+            movementPerSecond = -movementDirection * characterVelocity; // Inverses the direction on the movement Instead
+        }
+    }
+}
+
---
+++ Assets/Scripts/Player Scripts/Rattlesnake/RattlesnakeMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RattlesnakeMovement : MonoBehaviour
+{
+
+    public Animator myAnime;
+    public Rigidbody2D MyRig;
+    float Speed = 2.0f;
+    public int HP = 10;
+    public int MaxHP = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnime = this.GetComponent<Animator>();
+        MyRig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Basic Vertical Movement Code!
+        float v = Input.GetAxisRaw("Vertical");
+        MyRig.velocity = new Vector3(0, v, 0).normalized * Speed;
+
+
+        // Animation Controls  - Note Animations take Prioity based on order seen here
+        if (Input.GetAxisRaw("Fire1") > 0)
+        {
+            myAnime.SetInteger("DIR", 3); // works
+        }
+        else if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            myAnime.SetInteger("DIR", 1); // Not going here for some reason - SOLVED - Reason Why Was Because Of no else
+
+        }
+        else if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            myAnime.SetInteger("DIR", 2); // works
+
+        }
+        else if (Input.GetAxisRaw("Jump") > 0)
+        {
+            myAnime.SetInteger("DIR", 4); //sorta works
+
+        }
+        else
+        {
+            myAnime.SetInteger("DIR", 0); // works
+
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other) // Used For the Collision of the Bullets - CURRENT ISSUE - No Collisions Detected
+    {
+
+        if (other.gameObject.tag == "ENEMYBULLET")
+        {
+            Debug.Log("COLLISION ENEMY BULLET DETECTED");
+            HP = HP - 1; // put after sending it away 
+        }
+
+    }
+}
